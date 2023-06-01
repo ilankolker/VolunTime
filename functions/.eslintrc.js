85fd89b273c8bb816e7cbd15b464bad7264423a7
Ilan Kolker <ilan.kolker@maik.huji.ac.il>
2023-06-01 12:46:39 +0300

    Basic backend

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -4,16 +4,11 @@ module.exports = {
     node: true,
   },
   parserOptions: {
-    "ecmaVersion": 2018,
   },
   extends: [
-    "eslint:recommended",
-    "google",
   ],
   rules: {
-    "no-restricted-globals": ["error", "name", "length"],
-    "prefer-arrow-callback": "error",
-    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+
   },
   overrides: [
     {
--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,49 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// Send chat-gpt our query
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.generateVolunteers = functions.https.onCall(async (data, context) => {
+
+    age = data.age
+    location = data.location
+    hobbies = data.hobbies
+    //how far will you go to make a diffrenece?
+    availabilty = data.availabilty
+    
+
+    res = chatgpt.send(propt.replace("age", age, "location", location, "hobbies",
+                                     hobbies, "availabilty", availabilty))
+    
+    // add all volunteers to db if not exists
+
+
+    return res
+})
+
+// We get a key of the volunteering and user data and add the user to the volunnteering table
+
+// auth trigger (new join us)
+
+exports.addUserToVolunteering = functions.onCall(async (data, context) => {
+    admin.firestore().collection('volunteering').doc(data.volunteering.website, data.volunteering.location).set({
+
+        volunteers: volunteers.push(data.user.id)
+    })
+
+})
+
+// add a new volunteering if it doesnt exist
+
+exports.addVolunteering = functions.onCall(async (data, context) =>{
+    admin.firestore().collection('volunterring').doc(data.volunteering.website, data.volunteering.location).set({
+        website: data.volunteering.website,
+        location: data.volunteering.location,
+        name: data.volunteering.name,
+        description: data.volunteering.description,
+        volunteers: []
+    })
+
+})
